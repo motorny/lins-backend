ad451998eb6243ce5f38eb3877067704e8a8f597
Dmitriy Bragin <dmitriybragin@yandex.ru>
2019-10-06 19:15:34 +0300

    Add live reloading via webpack

--- routes/register.js
+++ routes/register.js
@@ -6,9 +6,13 @@ const router = express.Router();
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
+/* This method (at least for now) is getting fields from auth form */
 router.post("/", urlencodedParser, function (request, response) {
+    /* 1. Check for having something sent */
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
+    /* 2. Password is encrypted? Need to search in DB for that user and password */
+    /* End: Response sent to client */
     response.send(JSON.stringify(`${request.body.username} - ${request.body.password}`));
 });
 
