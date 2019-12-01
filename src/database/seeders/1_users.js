2a94cd9d8bfc5320b7e4d5d0145270c759f5e05d
motorny <motorny.nikita@gmail.com>
2019-12-01 19:05:18 +0300

    Changed password for seeders to qwerty

--- src/database/seeders/1_users.js
+++ src/database/seeders/1_users.js
@@ -1,76 +1,77 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    up: (queryInterface, Sequelize) => {
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-    return queryInterface.bulkInsert('users', [{
-      login: 'johndoe@yourmum.com',
-      password: 'encrypted_ting',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      isAdmin: false,
+          Example:
+          return queryInterface.bulkInsert('People', [{
+            name: 'John Doe',
+            isBetaMember: false
+          }], {});
+        */
+        return queryInterface.bulkInsert('users', [
+            {
+                login: 'johndoe@yourmum.com',
+                password: '$2a$10$DXYW9p9Mjs87X8xdGqU9eeRPUn8kshpd/GcTfwlvx5pxAR6T4.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                isAdmin: false,
+            },
+            {
+                login: 'ceo@lins.com',
+                password: '$2a$10$DXYW9p9Mjs87X8xdGqU9eeRPUn8kshpd/GcTfwlvx5pxAR6T4.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                isAdmin: true,
+            },
+            {
+                login: 'waitress@lins.com',
+                password: '$2a$10$DXYW9p9Mjs87X8xdGqU9eeRPUn8kshpd/GcTfwlvx5pxAR6T4.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                isAdmin: false,
+            },
+            {
+                login: 'plane@nasa.com',
+                password: '$2a$10$DXYW9p9Mjs87X8xdGqU9eeRPUn8kshpd/GcTfwlvx5pxAR6T4.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                isAdmin: false,
+            },
+            {
+                login: 'elon@musk.com',
+                password: '$2a$10$DXYW9p9Mjs87X8xdGqU9eeRPUn8kshpd/GcTfwlvx5pxAR6T4.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                isAdmin: false,
+            },
+            {
+                login: 'i@dont.know',
+                password: '$2a$10$DXYW9p9Mjs87X8xdGqU9eeRPUn8kshpd/GcTfwlvx5pxAR6T4.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                isAdmin: false,
+            },
+            {
+                login: 'omg@how.many',
+                password: '$2a$10$DXYW9p9Mjs87X8xdGqU9eeRPUn8kshpd/GcTfwlvx5pxAR6T4.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                isAdmin: false,
+            }], {});
     },
-      {
-        login: 'ceo@lins.com',
-        password: 'so_encrypted_ting',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        isAdmin: true,
-      },
-      {
-        login: 'waitress@lins.com',
-        password: 'qwerty',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        isAdmin: false,
-      },
-      {
-        login: 'plane@nasa.com',
-        password: 'sae6235asg@@$gst76',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        isAdmin: false,
-      },
-      {
-        login: 'elon@musk.com',
-        password: 'gint_detpyrcne',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        isAdmin: false,
-      },
-      {
-        login: 'i@dont.know',
-        password: 'bored_a_bit',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        isAdmin: false,
-      },
-      {
-        login: 'omg@how.many',
-        password: 'more',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        isAdmin: false,
-      }], {});
-  },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    down: (queryInterface, Sequelize) => {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+          Example:
+          return queryInterface.bulkDelete('People', null, {});
+        */
 
-  }
+    }
 };
